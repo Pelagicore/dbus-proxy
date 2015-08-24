62230f87096afd4f9276e68b61b9bf102f6086d7
Kristofer Hansson Aspman <kristofer.hansson-aspman@pelagicore.com>
2015-08-24 11:30:32 +0000

    Added test case capturing zombie process and fd leaks problem

---
+++ component-test/start_outside_service.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import dbus
+import dbus.service
+
+"""
+    This is an example service that connects to the
+    D-Bus session bus running on the system. It is used
+    for testing purposes and represents a service running
+    outside of a container.
+"""
+class SomeObject(dbus.service.Object):
+    def __init__(self):
+        # Connect to the D-Bus session bus running outside the container
+        self.__bus = dbus.bus.BusConnection('unix:path=/tmp/dbus_proxy_outside_socket')
+        name = dbus.service.BusName("com.dbusproxyoutsideservice.SampleService",
+                                    bus=self.__bus)
+        dbus.service.Object.__init__(self, name, '/SomeObject')
+
+    @dbus.service.method("com.dbusproxyoutsideservice.SampleInterface",
+                         in_signature='s', out_signature='as')
+    def HelloWorld(self, hello_message):
+        return ["Hello", "from unique name", self.__bus.get_unique_name()]
+
+    @dbus.service.method("com.dbusproxyoutsideservice.SampleInterface",
+                         in_signature='', out_signature='')
+    def Exit(self):
+        mainloop.quit()
+
+if __name__ == '__main__':
+    # using glib
+    import dbus.mainloop.glib
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    import gobject
+    loop = gobject.MainLoop()
+    object = SomeObject()
+    loop.run()
---
+++ component-test/test_dbus_proxy.py
@@ -0,0 +1,36 @@
+import pytest
+
+"""
+    Tests various aspects of the D-Bus proxy. Depending on the test case, the
+    test may act as a service running outside of the container or an app running
+    on the inside.
+"""
+class TestDBusProxy(object):
+    """
+        According to https://atlassian.pelagicore.net/jira/browse/TAC-58, the
+        D-Bus proxy had fd leaks and zombie processes making it crash after 544
+        calls. Using dbus-send command, this test attempts to call a D-Bus
+        service running outside the container. The test represents an app
+        running on the inside.
+        The test will send 1024 messages to a D-Bus proxy test service.
+
+        Configuration to use: conf_allow_all.json
+    """
+#    @pytest.mark.skipif(1, reason="Takes some time to execute")
+    def test_dbus_send_command(self):
+        from os import environ
+        from subprocess import Popen, PIPE
+
+        DBUS_SEND_CMD = ["dbus-send",
+                         "--address=unix:path=/tmp/dbus_proxy_inside_socket",
+                         "--print-reply",
+                         '--dest=com.dbusproxyoutsideservice.SampleService',
+                         "/SomeObject",
+                         "com.dbusproxyoutsideservice.SampleInterface.HelloWorld",
+                         'string:"Test says hello"']
+
+        environment = environ.copy()
+        for _x in range(0, 1024):
+            dbus_send_process = Popen(DBUS_SEND_CMD, env=environment, stdout=PIPE)
+            captured_stdout = dbus_send_process.communicate()[0]
+            assert "Hello" in captured_stdout
